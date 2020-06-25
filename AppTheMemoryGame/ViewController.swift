882476566892e1418ba4af720ae1d6d4d02f70b0
olivia <olivia.zhen3@gmail.com>
2020-06-25 17:11:49 -0500

    june 25 5:11

--- AppTheMemoryGame/ViewController.swift
+++ AppTheMemoryGame/ViewController.swift
@@ -103,9 +103,29 @@ weak var displayImage: UIImageView!
 
           
 
-    @IBAction func takePictureTapped(_ sender: UIButton) {
+    @IBAction func takePictureTapped(_ sender: Any) {imagePicker.sourceType = .camera
+        
+          present( imagePicker, animated : true, completion : nil)
+        
+    }
+   
+        
+    @IBAction func photoLibraryTapped(_ sender: Any) { imagePicker.sourceType = .photoLibrary
+        
+         present( imagePicker, animated : true, completion : nil)
+        
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage ] as? UIImage {displayImage.image = selectedImage
+    }
+
+        imagePicker.dismiss(animated : true, completion : nil)
+}
+    
+
+    
     // CHATBOT CODE
+
 
 }
